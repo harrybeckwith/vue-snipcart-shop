8f5efe9aa4062ecfbd5ce7304a467e297e165c43
harrybeckwith <harryjbeckwith@gmail.com>
2021-02-02 10:53:04 +0000

    add all

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <HelloWorld />
+  <div
+    hidden
+    id="snipcart"
+    data-api-key="NWE5OWVhMjYtYTNmMi00MWE1LWFkMzMtYWMwNTk5ZjJlZDFiNjM3NDc2MDE2MDIzOTIyMjk2"
+  ></div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <button
+      class="snipcart-add-item"
+      data-item-id="starry-night"
+      data-item-price="79.99"
+      data-item-url="/paintings/starry-night"
+      data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
+      data-item-image="/assets/images/starry-night.jpg"
+      data-item-name="The Starry Night"
+    >
+      Add to cart
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
